588517f5f3441ec7c5ef350cc0f2f951d31469f5
Hadixp <hadi.b.2002@gmail.com>
2025-01-11 02:34:20 +0330

    Feat: added real api
    I added real weather api

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ btn.addEventListener('click', loadData);
 // ajax function
 function loadData(){
     const xhr = new XMLHttpRequest();
-    const url = 'https://catfact.ninja/fact';
+    const apiKey = 'd4f16e53afe04283826224619251001';
+    const endPoint = 'https://api.weatherapi.com/v1/current.json';
+    const location = 'Borazjan';
+    const url = `${endPoint}?key=${apiKey}&q=${location}`;
     xhr.open('GET',url);
     xhr.onload = function(){
         const dataApi = JSON.parse(this.response);
-        console.log(dataApi.fact);
-        data.textContent = dataApi.fact;
+        console.log(dataApi.current.temp_c);
+        data.textContent = dataApi.current.temp_c;
     }
     xhr.send();
 }
